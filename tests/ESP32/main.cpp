b084af7f766f4683335daa0d59ad66eb6c4a6c33
Todor Vasilev <toschomacher@gmail.com>
2026-04-17 15:46:25 +0100

    Add sendCAN and taskButtons methods.

--- tests/ESP32/main.cpp
+++ tests/ESP32/main.cpp
@@ -29,4 +29,43 @@ float voltage = 14.4;
 bool prevUp = HIGH;
 bool prevDown = HIGH;
 
+// ----------------------
+// CAN SEND
+// ----------------------
+void sendCAN(uint32_t id, uint8_t *data) {
+  twai_message_t msg;
+  msg.identifier = id;
+  msg.extd = 0;
+  msg.rtr = 0;
+  msg.data_length_code = 8;
+  memcpy(msg.data, data, 8);
+  twai_transmit(&msg, pdMS_TO_TICKS(10));
+}
+
+// ----------------------
+// BUTTON TASK
+// ----------------------
+void taskButtons(void *pv) {
+  while (true) {
+
+    brake  = (digitalRead(BTN_BRAKE) == LOW);
+    clutch = (digitalRead(BTN_CLUTCH) == LOW);
+
+    bool up = digitalRead(BTN_COOLANT_UP);
+    if (prevUp == HIGH && up == LOW) {
+      coolantC += 10;
+    }
+    prevUp = up;
+
+    bool down = digitalRead(BTN_COOLANT_DOWN);
+    if (prevDown == HIGH && down == LOW) {
+      coolantC -= 10;
+      if (coolantC < 0) coolantC = 0;
+    }
+    prevDown = down;
+
+    vTaskDelay(pdMS_TO_TICKS(20));
+  }
+}
+
 void loop() {}
